cc533b7f9dd13a2afa6727bfdfe1a0594b0d4f88
codemmk <2107228359@qq.com>
2024-08-05 23:47:59 +0800

    根据实际数据渲染组件

--- app/ui/Menu/index.jsx
+++ app/ui/Menu/index.jsx
@@ -1,22 +1,48 @@
-import { memo } from "react";
-import styles from './menu.module.css'
+'use client';
 
+import { memo, useState } from "react";
+import styles from './menu.module.css'
 const Menu = memo(() => {
+
+  const [navgation] = useState([
+    {
+      id: 1,
+      name: 'Blog',
+      path: '/blog'
+    },
+    {
+      id: 2,
+      name: 'Category',
+      path: '/category'
+    },
+    {
+      id: 3,
+      name: 'About',
+      path: '/about'
+    }
+  ])
+
+  const [active, setActive] = useState(1)
+
   return (
     <div className="h-full w-64 bg-[#0c0b24]">
       <h1 className="text-[#b082e7] font-bold text-xl h-16 flex justify-center items-center">Milk Homepage</h1>
 
       <nav className="ml-6 mt-6">
         <ul>
-          <li className={`mb-4 text-white cursor-pointer hover:text-[#b082e7] ${styles.active}`}>
-            <span>Blog</span>
-          </li>
-          <li className="mb-4 text-white cursor-pointer hover:text-[#b082e7]">
-            <span>Category</span>
-          </li>
-          <li className="mb-4 text-white cursor-pointer hover:text-[#b082e7]">
-            <span>About</span>
-          </li>
+          {
+            navgation.map(item => {
+              return (
+                <li 
+                  className={`mb-4 text-white cursor-pointer hover:text-[#b082e7] ${active === item.id ? styles.active : ''}`}
+                  key={item.id}
+                  onClick={ () => setActive(item.id) }
+                >
+                  <span>{ item.name }</span>
+                </li>
+              )
+            })
+          }
         </ul>
       </nav>
     </div>
