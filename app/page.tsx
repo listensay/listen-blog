c26ef87409c6993123c7dfc99f8efad3892794fb
codemmk <2107228359@qq.com>
2024-08-05 20:33:30 +0800

    use css module

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Menu from './ui/Menu'
 import Header from './ui/Header'
 import Sider from './ui/Sider'
+import styles from './page.module.css'
 export default function Home() {
   return (
     <main className="wrap-base bg h-[90vh] my-auto">
@@ -11,8 +12,8 @@ export default function Home() {
           <div className='flex justify-between'>
             <div className='flex-1'>
               <div className='w-full h-80 bg-[#3C37D2] p-8'>
-                <h2 className='text-3xl mb-2'>Welcom to</h2>
-                <h2 className='text-3xl mb-2'>Milk Homepage</h2>
+                <h2 className={`text-6xl mb-2 ${ styles.cute }`}>Welcom to</h2>
+                <h2 className={`text-6xl mb-2 ${ styles.cute }`}>Milk Homepage</h2>
                 <div className='text-sm text-gray-300'>Listen a 22 years old boy story</div>
               </div>
             </div>
--- app/ui/Menu/index.jsx
+++ app/ui/Menu/index.jsx
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import styles from './menu.module.css'
 
 const Menu = memo(() => {
   return (
@@ -7,7 +8,7 @@ const Menu = memo(() => {
 
       <nav className="ml-6 mt-6">
         <ul>
-          <li className="mb-4 text-white cursor-pointer hover:text-[#b082e7]">
+          <li className={`mb-4 text-white cursor-pointer hover:text-[#b082e7] ${styles.active}`}>
             <span>Blog</span>
           </li>
           <li className="mb-4 text-white cursor-pointer hover:text-[#b082e7]">
